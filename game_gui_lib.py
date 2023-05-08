d731e159aadf4c50d719ad1ad2bc9ca7bd650c3f
hermey <49494077+hermy1@users.noreply.github.com>
2023-05-08 17:34:54 -0400

    fixed ball going off top screen

--- game_gui_lib.py
+++ game_gui_lib.py
@@ -71,48 +71,43 @@ class BrickGameFrame(AnimatedGameFrame):
 		self.points = 0
 		self.gameover = False
 		self.stop()
-	
+
+		#game background
 		self.bg_sprite = Sprite(0,0,canvas_width,canvas_height,image=self.bg_image)
 		self.bg_sprite.draw(self.canvas)
 
+		#game objects
+		self.ball = Ball(canvas_width // 2, canvas_height // 2, 10, 'white')
+		self.paddle = Paddle(canvas_width // 2 - 50, canvas_height - 50, 100, 10, 'white', canvas_width)
+		self.bricks = []
+		for i in range(10):
+			for j in range(5):
+				brick = Brick(i * 80 + 10, j * 30 + 50, 70, 20, f'#{randint(0, 0xFFFFFF):06x}')
+				self.bricks.append(brick)
 
-		self.hero = AnimatedHorizontalMovingSprite(self.player_images['Left'],self.player_images['Right'],
-												   self.canvas_width//2,self.canvas_height - 50,border_width=0)
-		self.hero.draw(self.canvas)
-		self.coins = AnimatedRandomFallingObjects(self.coin_images,bottom_limit=750)
-
-		
 		self.canvas.create_text(self.canvas_width // 2, self.canvas_height // 2,
 								font=("Comic Sans MS", self.start_game_message_font_size),
 								text=self.start_game_message, fill='white')
-		self.ball = Ball(canvas=self,x=250,y=250,radius=10,direction=[1,-1],speed=5)
+
 		
-		
-		self.drawables = [self.bg_sprite,self.hero, self.coins,self.ball]
-		self.updateables = [self.hero, self.coins,self.ball]
-		
-		
-		
-		
-		
+		#add items to canvas
+		self.drawables = [self.bg_sprite,self.paddle, self.ball,*self.bricks]
+		self.updateables = [self.ball]
+
+
 	def bind_keys(self):
-		self.root = self.winfo_toplevel()
-		self.root.bind("<Left>", self.direction_handler)
-		self.root.bind("<Right>", self.direction_handler)
-		self.root.bind("p", self.toggle_play)
-		self.root.bind('s', self.speed_up)
-		self.root.bind('a', self.reduce_speed)
-		self.root.bind('y', self.reset_game)
-		self.root.bind('n', self.quit)
+			self.root = self.winfo_toplevel()
+			self.root.bind("<Left>", self.direction_handler)
+			self.root.bind("<Right>", self.direction_handler)
+			self.root.bind("p", self.toggle_play)
+			self.root.bind('s', self.speed_up)
+			self.root.bind('a', self.reduce_speed)
+			self.root.bind('y', self.reset_game)
+			self.root.bind('n', self.quit)
 		
 	def load_assets(self):
 		self.images = ImageHelper.slice_to_list("images/alien.png", 4, 4, 50, 50)
-		self.player_images = dict({
-			'Left': self.images[4:8],
-			'Right': self.images[8:12],
-			'Up': self.images[12:16],
-			'Down': self.images[0:4]})
-		self.coin_images = ImageHelper.slice_to_list("images/electric_ball_sheet.png", 9, 1, 35, 35)
+
 		self.bg_image = ImageHelper.get_sized_image('images/moon_bg.jpg',self.canvas_width,self.canvas_height)
 		
 	def quit(self,evt=None):
@@ -121,36 +116,36 @@ class BrickGameFrame(AnimatedGameFrame):
 	def reset_game(self,evt=None):
 		print('reset')
 		self.load_assets()
-		
+		self.ball = Ball(self.canvas_width // 2, self.canvas_height // 2, 10, 'white')
+		self.paddle = Paddle(self.canvas_width // 2 - 50, self.canvas_height - 50, 100, 10, 'white', self.canvas_width)
+		self.bricks = []
+		for i in range(10):
+			for j in range(5):
+				brick = Brick(i * 80 + 10, j * 30 + 50, 70, 20, f'#{randint(0, 0xFFFFFF):06x}')
+				self.bricks.append(brick)
+		self.drawables = [self.bg_sprite,self.paddle, self.ball,*self.bricks]
+		self.updateables = [self.ball]
 		self.lives = 3
 		self.points = 0
 		self.gameover = False
 		self.stop()
-		
+		self.canvas.delete('all')
 		self.bg_sprite.draw(self.canvas)
-		
-		self.hero.sprite.center_x = self.canvas_width//2
 
-		self.hero.draw(self.canvas)
-		for coin in self.coins.objects:
-			coin.reset_position()
+
 		
 		self.canvas.create_text(self.canvas_width // 2, self.canvas_height // 2,
 								font=("Comic Sans MS", self.start_game_message_font_size),
 								text=f'Press Left/Right\nArrows to Begin', fill='white')
 	
 	def speed_up(self,evt):
+		pass
 		
-		self.hero.mover.speed += 2
-		print(self.hero.mover.speed)
-		if self.hero.mover.delay_time > self.delay_time:
-			self.hero.mover.delay_time -= 5
+
 		
 	def reduce_speed(self,evt):
-		if self.hero.mover.speed > 2:
-			self.hero.mover.speed -= 2
-		self.hero.mover.delay_time += 5
-		
+		pass
+
 	def toggle_play(self,evt):
 		if self.is_paused:
 			self.start()
@@ -161,29 +156,54 @@ class BrickGameFrame(AnimatedGameFrame):
 		if self.is_paused and not self.gameover:
 			self.start()
 		if evt.keysym == 'Left':
-			self.hero.mover.direction = Direction.LEFT
+			self.paddle.move_left()
 		elif evt.keysym == 'Right':
-			self.hero.mover.direction = Direction.RIGHT
-		elif evt.keysym == 'Up':
-			self.hero.mover.direction = Direction.UP
-		elif evt.keysym == 'Down':
-			self.hero.mover.direction = Direction.DOWN
+			self.paddle.move_right()
+
+
+
 	
 	def update(self):
 		super().update()
-				
-		intersections = self.coins.intersects(self.hero.sprite.bbox())
-		self.points += len(intersections)
-		#self.get_position(self.hero.sprite)
-		for obj in intersections:
-			obj.reset_position()
-		for coin in self.coins.objects:
-			if coin.sprite.top > self.canvas_height:
-				self.lives -= 1
-				coin.reset_position()
-				if self.lives <= 0:
-					self.gameover = True
-					self.lives = 0
+		for brick in self.bricks:
+			if brick.check_collision(self.ball):
+				self.points += 10
+		# Check if the ball hits the bottom edge
+		if self.ball.y + self.ball.radius >= self.canvas_height:
+			self.lives -= 1
+			self.ball.reset()
+			# self.paddle.reset()
+			if self.lives == 0:
+				self.gameover = True
+				self.stop()
+				self.canvas.create_text(self.canvas_width // 2, self.canvas_height // 2,
+										font=("Comic Sans MS", self.game_over_message_font_size),
+										text=f'Game Over', fill='white')
+
+
+		#check if the ball hits the paddle
+		if self.ball.check_collision(self.paddle):
+			self.ball.dy *= -1
+			self.ball.y = self.paddle.y - self.ball.radius
+
+
+		# Check if the ball hits the left or right edge
+		if self.ball.x + self.ball.radius >= self.canvas_width or self.ball.x - self.ball.radius <= 0:
+			self.ball.dx *= -1
+		# Check if the ball hits the top edge
+		if self.ball.y - self.ball.radius <= 0:
+			self.ball.dy *= -1
+
+
+		# Check if the ball hits the paddle
+	    # if self.ball.y - self.ball.radius <= 0 or self.ball.check_collision(self.paddle):
+	    #     self.ball.dy *= -1
+
+
+	# Check if the ball falls off the bottom of the frame
+		#check for intersection/collision
+		#reduce lives when the player looses
+		#reset ball position etc
 					
 					
 	def draw(self):
--- game_lib.py
+++ game_lib.py
@@ -1,42 +1,89 @@
-from tkinter import *
+from random import randint
+
 class Brick:
-    pass
-class Ball:
-    def __init__(self, canvas, x, y, radius, direction, speed, color='white'):
-        self.canvas = canvas
-        self.radius = radius
-        self.direction = direction
-        self.speed = speed
+    def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
 
-        # self.item = canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=color)
+    def draw(self, canvas):
+        canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, fill=self.color, outline='white')
 
-    def get_position(self):
-        return self.canvas.coords(self.item)
+    def check_collision(self, ball):
+        if (ball.x + ball.radius > self.x and ball.x - ball.radius < self.x + self.width and
+                ball.y + ball.radius > self.y and ball.y - ball.radius < self.y + self.height):
+            # collision detected
+            ball.dy *= -1  # reverse y-direction of ball
+            self.x = -1000  # move brick off screen
+            self.y = -1000
+            return True
+        return False
 
-    def move(self, x, y):
-        self.canvas.move(self.item, x, y)
 
-    def draw(self,canvas):
-        canvas.create_oval(self.x - self.radius, self.y - self.radius, self.x + self.radius, self.y + self.radius)
+class Ball:
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.dx = randint(-5, 5)
+        self.dy = -5
 
-    def update(self,dt):
-        coords = self.get_position()
-        width = self.canvas.winfo_width()
-        if coords[0] <= 0 or coords[2] >= width:
-            self.direction[0] *= -1
-        if coords[1] <= 0:
-            self.direction[1] *= -1
-        x = self.direction[0] * self.speed
-        y = self.direction[1] * self.speed
-        self.move(x, y)
 
-    def collide(self, game_objects):
-        coords = self.get_position()
-        x = (coords[0] + coords[2]) / 2
-        y = (coords[1] + coords[3]) / 2
+    def draw(self, canvas):
+        canvas.create_oval(self.x - self.radius, self.y - self.radius, self.x + self.radius, self.y + self.radius, fill=self.color)
 
-        for game_object in game_objects:
-            if isinstance(game_object, Brick):
-                game_object.on_collide(self, x, y)
+    def update(self, dt):
+        self.x += self.dx
+        self.y += self.dy
+        # Check if the ball hits the left or right edge
+        # Check if the ball hits the top edge
+        # Check if the ball hits the paddle
+
+        # Check if the ball falls off the bottom of the frame
+
+    def reset(self):
+        self.x = 300
+        self.y = 300
+        self.dx = randint(-5, 5)
+        self.dy = -5
+    def check_collision(self, paddle):
+        if (self.x + self.radius > paddle.x and self.x - self.radius < paddle.x + paddle.width and
+                self.y + self.radius > paddle.y and self.y - self.radius < paddle.y + paddle.height):
+            # collision detected
+            self.dy *= -1
+
+
+class Paddle:
+    def __init__(self, x, y, width, height, color, canvas_width):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.canvas_width = canvas_width
+        self.ball = None
+
+    def draw(self, canvas):
+        canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, fill=self.color, outline='white')
+
+    def move_left(self):
+        self.x = max(self.x - 40, 0)
+
+    def move_right(self):
+        self.x = min(self.x + 40, self.canvas_width - self.width)
+
+
+
+
+
+
+    # def reset(self):
+    #     self.x = 300
+    #     self.y = 300
+    #     self.dx = randint(-5, 5)
+    #     self.dy = -5
+
+
